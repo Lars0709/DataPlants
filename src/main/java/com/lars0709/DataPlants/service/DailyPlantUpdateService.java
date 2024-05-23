f337c784cc7778efdda6b61276a4d7a90a1fe638
Lars Nicht <larsnicht@web.de>
2024-05-23 09:34:11 +0200

    removed unused imports

--- src/main/java/com/lars0709/DataPlants/service/DailyPlantUpdateService.java
+++ src/main/java/com/lars0709/DataPlants/service/DailyPlantUpdateService.java
@@ -5,6 +5,7 @@ import com.lars0709.DataPlants.repository.DailyPlantUpdateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
