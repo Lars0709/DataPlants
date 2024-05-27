9ec666505a328d5b2dfc9918d181369803cde687
Lars0709 <larsnicht@web.de>
2024-05-27 20:38:54 +0200

    added filter, search and sort function for daily updates

--- src/main/java/com/lars0709/DataPlants/repository/DailyPlantUpdateRepository.java
+++ src/main/java/com/lars0709/DataPlants/repository/DailyPlantUpdateRepository.java
@@ -3,9 +3,10 @@ package com.lars0709.DataPlants.repository;
 import com.lars0709.DataPlants.model.DailyPlantUpdate;
 import com.lars0709.DataPlants.model.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface DailyPlantUpdateRepository extends JpaRepository<DailyPlantUpdate, Long> {
+public interface DailyPlantUpdateRepository extends JpaRepository<DailyPlantUpdate, Long>, JpaSpecificationExecutor<DailyPlantUpdate> {
     List<DailyPlantUpdate> findByPlant(Plant plant);
 }
--- src/main/java/com/lars0709/DataPlants/service/DailyPlantUpdateService.java
+++ src/main/java/com/lars0709/DataPlants/service/DailyPlantUpdateService.java
@@ -4,8 +4,9 @@ import com.lars0709.DataPlants.model.DailyPlantUpdate;
 import com.lars0709.DataPlants.repository.DailyPlantUpdateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Service
